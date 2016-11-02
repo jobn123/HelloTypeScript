5cedd0e8d852b1ac80fbf0719bf5c8579d4e973d
bin zhou <jobn_joy@163.com>
2016-11-02 17:21:03 +0800

    finish interface

--- interface.ts
+++ interface.ts
@@ -44,3 +44,64 @@ interface StringArray {
 
 var myArray: StringArray;
 myArray = ["Bob", "Fred"];
+
+/**类实现一个接口*/
+//接口仅仅用于定义一个类中的公有部分。用接口来定义类的私有属性/方法，都是不允许的。
+interface ClockInterface {
+  currtime: Date;
+  setTime(d: Date);
+}
+
+class Clock implements ClockInterface {
+  currtime: Date;
+  setTime(d: Date) {
+      this.currtime = d;
+  }
+  constructor(h: number, m:number) {
+  }
+}
+
+//当类实现一个接口时，只有类的实例部分才会被检查。
+//由于构造函数是类的静态部分，它将不会被检查。
+interface ClockStatic {
+    new (hour: number, minute: number);
+}
+
+class Clock  {
+    currentTime: Date;
+    constructor(h: number, m: number) { }
+}
+
+var cs: ClockStatic = Clock;
+var newClock = new cs(7, 30);
+
+/**接口继承*/
+//和类一样，接口也可以相互继承,也可以继承多个接口：
+interface Shape {
+    color: string;
+}
+
+interface PenStroke {
+    penWidth: number;
+}
+
+interface Square extends Shape, PenStroke {
+    sideLength: number;
+}
+
+var square = <Square>{};
+square.color = "blue";
+square.sideLength = 10;
+square.penWidth = 5.0;
+
+/**混合类型*/
+interface Counter {
+    (start: number): string;
+    interval: number;
+    reset(): void;
+}
+
+var c: Counter;
+c(10);
+c.reset();
+c.interval = 5.0;
